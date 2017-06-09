6b169a423427970c8a7eb2ac2dce620e35fc0277
ankedittmer <anke.t.dittmer@gmail.com>
2017-06-09 16:30:41 +0200

    Trying to modify for our shape-dataset use

--- InfoGAN/infogan/misc/datasets.py
+++ InfoGAN/infogan/misc/datasets.py
@@ -8,10 +8,13 @@ class Dataset(object):
     def __init__(self, images, labels=None):
         self._images = images.reshape(images.shape[0], -1)
         self._labels = labels
+
         self._epochs_completed = -1
         self._num_examples = images.shape[0]
         # shuffle on first run
         self._index_in_epoch = self._num_examples
+        self.image_dim = 28 * 28
+        self.image_shape = (28, 28, 1)
 
     @property
     def images(self):
@@ -85,3 +88,59 @@ class MnistDataset(object):
 
     def inverse_transform(self, data):
         return data
+
+class ShapeDataset(object):
+    def __init__(self):
+        data_directory = "SHAPE"
+        if not os.path.exists(data_directory):
+            os.makedirs(data_directory)
+        dataset = mnist.input_data.read_data_sets(data_directory)
+
+
+        self._images = images.reshape(images.shape[0], -1)
+        self._labels = labels
+        self.image_dim = 28 * 28
+        self.image_shape = (28, 28, 1)
+        self._epochs_completed = -1
+        self._num_examples = images.shape[0]
+        # shuffle on first run
+        self._index_in_epoch = self._num_examples
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self, batch_size):
+        """Return the next `batch_size` examples from this data set."""
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Shuffle the data
+            perm = np.arange(self._num_examples)
+            np.random.shuffle(perm)
+            self._images = self._images[perm]
+            if self._labels is not None:
+                self._labels = self._labels[perm]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size
+            assert batch_size <= self._num_examples
+        end = self._index_in_epoch
+        if self._labels is None:
+            return self._images[start:end], None
+        else:
+            return self._images[start:end], self._labels[start:end]
