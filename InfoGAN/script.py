cd0e470ede47696d21c4c56f0dcf72e610774ae8
Felix Blind <felixblind@gmail.com>
2017-08-09 14:09:03 +0200

    deleting 1, script.py, test

--- InfoGAN/script.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import numpy as np
-
-__liste__ = pickle.load(open('images.pkl', 'rb'))
-
-trainMatrixSample = np.array(__liste__[0])
-trainLabelsSample = np.array(__liste__[1])
-testMatrixSample = np.array(__liste__[2])
-testLabelsSample = np.array(__liste__[3])
